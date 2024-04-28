35847298e72640d0d4050eb51355c0ef897f5335
Nicolas Perrault <nipe1783@colorado.edu>
2024-04-28 14:41:18 -0600

    Test

--- driveLap/driveLap/driveLap.py
+++ driveLap/driveLap/driveLap.py
@@ -23,8 +23,6 @@ class DriveLap(Node):
 
 		forward_distance_ = msg.ranges[0]
 		right_distance_ = msg.ranges[398]
-		top_right_distance_ = msg.ranges[486]
-		top_left_distance = msg.ranges[44]
 		left_distance_ = msg.ranges[132]
 		self.get_logger().info(f'Forward distance: {forward_distance_:.2f} meters')
 		self.get_logger().info(f'Right distance: {right_distance_:.2f} meters')
