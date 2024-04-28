e6cec5ba723eb21506dee4507c455ab58997c2f9
Nicolas Perrault <nipe1783@colorado.edu>
2024-04-28 13:20:56 -0600

    test

--- driveLap/driveLap/driveLap.py
+++ driveLap/driveLap/driveLap.py
@@ -16,37 +16,22 @@ class DriveLap(Node):
 			ServoCtrlMsg,
 			'/webserver_pkg/manual_drive',
 			10)
-		self.driving_forward = True
-		self.lidar_buffer = deque(maxlen=50) #Buffer to store the last 50 LIDAR readings
+		self.forward_distance_ = 0.0
+		self.right_distance_ = 0.0
+		self.left_distance_ = 0.0
 
 	def lidar_callback(self, msg):
-		#Update lidar buffer with current readings
-		self.lidar_buffer.append(msg.ranges)
 
-		#Analyze lidar buffer to detect corners and intersections
-		recent_readings = list(self.lidar_buffer)[-10:]
-		if (not self.detect_intersection(recent_readings) and not self.detect_corner(recent_readings)):
-			self.drive_forward(msg)
-		elif self.detect_intersection(recent_readings):
-			self.go_straight(msg)
-		elif self.detect_corner(recent_readings):
-			self.turn(msg)
+		forward_distance_ = msg.ranges[0]
+		right_distance_ = msg.ranges[398]
+		left_distance_ = msg.ranges[132]
 
-	def get_forward_distance(self, scan: LaserScan):
-		forward_distance = scan.ranges[0]
-		if forward_distance == float('inf') or forward_distance == 0.0:
-			return -1.0
-		return forward_distance 
-
-	def detect_intersection(self, readings):
-		#Detect if it is an intersection based on the pattern of readings
-		#Implement logic
-		pass
-
-	def detect_corner(self, readings):
-		#Detect if it is a corner based on the pattern of readings
-		#Implement logic
-		pass
+		if(forward_distance_ > 5 and right_distance_ > 5):
+			self.go_right()
+		elif(forward_distance_ > 5 and right_distance_ > 0.5 and right_distance_ < 1):
+			self.go_straight()
+		else:
+			self.stop()
 
 	def go_straight(self, msg):
 		input = ServoCtrlMsg()
@@ -54,17 +39,14 @@ class DriveLap(Node):
 		input.throttle = 0.8
 		self.cmd_vel_publisher.publish(input)
 
-	def turn(self, msg):
-		#Implemetn turn algorithm
-		pass
+	def go_right(self, msg):
+		input = ServoCtrlMsg()
+		input.angle = -0.5
+		input.throttle = 0.8
+		self.cmd_vel_publisher.publish(input)
 
-	def drive_forward(self, msg):
-		forward_distance = self.get_forward_distance(msg)
-		self.get_logger().info(f'Forward distance: {forward_distance:.2f} meters')
+	def stop(self, msg):
 		input = ServoCtrlMsg()
 		input.angle = 0.0
-		if forward_distance > 1.0:
-			input.throttle = 0.8
-		else:
-			input.throttle = 0.0
+		input.throttle = 0.0
 		self.cmd_vel_publisher.publish(input)
