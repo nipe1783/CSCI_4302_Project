1d3b8e168a61259e42758ea86dc8525d9b99b4bb
Nicolas Perrault <nipe1783@colorado.edu>
2024-04-28 14:32:01 -0600

    test

--- driveLap/driveLap/driveLap.py
+++ driveLap/driveLap/driveLap.py
@@ -26,9 +26,9 @@ class DriveLap(Node):
 		top_right_distance_ = msg.ranges[486]
 		top_left_distance = msg.ranges[44]
 		left_distance_ = msg.ranges[132]
-		# self.get_logger().info(f'Forward distance: {forward_distance_:.2f} meters')
-		# self.get_logger().info(f'Right distance: {right_distance_:.2f} meters')
-		# self.get_logger().info(f'Left distance: {left_distance_:.2f} meters')
+		self.get_logger().info(f'Forward distance: {forward_distance_:.2f} meters')
+		self.get_logger().info(f'Right distance: {right_distance_:.2f} meters')
+		self.get_logger().info(f'Left distance: {left_distance_:.2f} meters')
 
 		if(forward_distance_ > 2 and right_distance_ > 1):
 			print("Right")
