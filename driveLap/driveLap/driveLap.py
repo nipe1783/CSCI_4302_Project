bc9f0fa6983af604fb8cd3617bbf2ba8d3ef00ba
mimu5583 <89423590+mimu5583@users.noreply.github.com>
2024-04-29 00:19:44 -0600

    Update driveLap.py

--- driveLap/driveLap/driveLap.py
+++ driveLap/driveLap/driveLap.py
@@ -28,13 +28,13 @@ class DriveLap(Node):
 		self.get_logger().info(f'Right distance: {right_distance_:.2f} meters')
 		self.get_logger().info(f'Left distance: {left_distance_:.2f} meters')
 
-		if (forward_distance < 1.0 and right_distance < 1.0):
+		if (forward_distance_ < 1.0 and right_distance_ < 1.0):
 			print("Left")
 			self.go_left()
-		elif (forward_distance > 1.0 and (right_distance > 0.75 and right_distance < 1.0)):
+		elif (forward_distance_ > 1.0 and (right_distance_ > 0.75 and right_distance_ < 1.0)):
 			print("Straight")
 			self.go_forward()
-		elif (forward_distance > 1.0 and right_distance > 1.0):
+		elif (forward_distance_ > 1.0 and right_distance_ > 1.0):
 			print("Right")
 			self.go_right()
 
