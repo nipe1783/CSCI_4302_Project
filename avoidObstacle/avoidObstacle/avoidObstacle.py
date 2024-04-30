ace540b08720672062fc1e206559c6f5a19b4e46
mimu5583 <89423590+mimu5583@users.noreply.github.com>
2024-04-30 15:39:24 -0600

    Update avoidObstacle.py

--- avoidObstacle/avoidObstacle/avoidObstacle.py
+++ avoidObstacle/avoidObstacle/avoidObstacle.py
@@ -2,10 +2,12 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from deepracer_interfaces_pkg.msg import ServoCtrlMsg
-
-class avoidObstacle(Node):
+import time
+# Get the current time in seconds since the epoch (January 1, 1970)
+current_time = time.time()
+class DriveLap(Node):
 	def __init__(self):
-		super().__init__('avoidObstacle')
+		super().__init__('driveLap')
 		self.subscription = self.create_subscription(
 			LaserScan,
 			'/rplidar_ros/scan',
@@ -15,5 +17,88 @@ class avoidObstacle(Node):
 			ServoCtrlMsg,
 			'/webserver_pkg/manual_drive',
 			10)
+		self.psi = 0
+		self.th = 0
+		self.df_cut = 0.2
+		self.th_max = 0.8
+		self.ds_min = 0.2
+		self.psi_max = 1
+		self.psi_min = -1
+		self.t_start = time.time()
+		self.t_prev = time.time()
+		self.ds_prev = 0
+		self.psi_acc = 0
+	def lidar_callback(self, msg):
+		df = msg.ranges[0]
+		#original true right should be 398
+		dr = msg.ranges[408]
+		#original left should be 132
+		dl = msg.ranges[122]
+		ds = dl-dr
+		self.get_logger().info(f'Forward distance: {df:.2f} meters')
+		self.get_logger().info(f'Right distance: {dr:.2f} meters')
+		self.get_logger().info(f'Left distance: {dl:.2f} meters')
+		#PID gains
+		k_th_p = 1
+		k_th_d = 0
+		k_th_i = 0
+		k_psi_p = 1
+		k_psi_d = 0
+		k_psi_i = 0
+		#P
+		if df < self.df_cut:
+			th_p = 0
+		else:
+			th_p = df
+		if dl < self.ds_min or dr < self.ds_min:
+			th_p = 0
+			psi_p = 0
+		else:
+			psi_p = ds
+		#D
+		t = time.time()
+		dt = t-self.t_prev
+		th_d = 0
+		psi_d = (ds-self.ds_prev)/dt
+		#I
+		psi_i = self.psi_acc+ds*dt
+		
+		# update prev vals
+		self.ds_prev = ds
+		self.t_prev = t
+		self.psi_acc = psi_i
+		#apply gain and sum
+		self.th = th_p*k_th_p + th_d*k_th_p + th_i*k_th_i
+		self.psi = psi_p*k_psi_p + psi_d*k_psi_p + psi_i*k_psi_i
+		#cuttoff
+		if self.th > self.th_max:
+			self.th = self.th_max
+		if self.psi > self.psi_max:
+			self.psi = self.psi_max
+		elif self.psi < self.psi_min:
+			self.psi = self.psi_min
+	def act(self):
+		input = ServoCtrlMsg()
+		input.angle = self.psi
+		input.throttle = self.th
+		self.cmd_vel_publisher.publish(input)
+def main(args = None):
+    rclpy.init(args=args)
+    node = DriveLap()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        node.stop()
+    finally:
+        # Destroy the node explicitly
+        node.destroy_node()
+        rclpy.shutdown()
+if __name__ == '__main__':
+    main()
 
-	def 
+
+
+
+
+
+
