b6f34df4d67fcf43cc11f6e112b87e971a454837
mimu5583 <89423590+mimu5583@users.noreply.github.com>
2024-05-02 16:32:37 -0600

    Update driveLap.py

--- driveLap/driveLap/driveLap.py
+++ driveLap/driveLap/driveLap.py
@@ -63,25 +63,25 @@ class DriveLap(Node):
 	def go_straight(self):
 		input = ServoCtrlMsg()
 		input.angle = 0.0
-		input.throttle = 0.6
+		input.throttle = 0.4
 		self.cmd_vel_publisher.publish(input)
 
 	def go_right(self):
 		input = ServoCtrlMsg()
 		input.angle = -0.9
-		input.throttle = 0.6
+		input.throttle = 0.4
 		self.cmd_vel_publisher.publish(input)
 	
 	def go_left(self):
 		input = ServoCtrlMsg()
 		input.angle = 0.9
-		input.throttle = 0.6
+		input.throttle = 0.4
 		self.cmd_vel_publisher.publish(input)
 
 	def stabilize(self):
 		input = ServoCtrlMsg()
-		input.angle = 0.2
-		input.throttle = 0.6
+		input.angle = 0.1
+		input.throttle = 0.4
 		self.cmd_vel_publisher.publish(input)
 
 	def stop(self):
