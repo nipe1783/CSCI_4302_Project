089541d68e34faa157af1790797182bab6bcf988
mimu5583 <89423590+mimu5583@users.noreply.github.com>
2024-05-02 16:25:59 -0600

    Update driveLap.py

--- driveLap/driveLap/driveLap.py
+++ driveLap/driveLap/driveLap.py
@@ -21,11 +21,11 @@ class DriveLap(Node):
 		self.cur_dir = "none"
 
 	def lidar_callback(self, msg):
-		forward_distance = msg.ranges[265]
-		right_45 = msg.ranges[199]
-		right_distance = msg.ranges[132]
-		left_45 = msg.ranges[332]
-		left_distance = msg.ranges[398]
+		forward_distance = msg.ranges[0]
+		right_45 = msg.ranges[464]
+		right_distance = msg.ranges[398]
+		left_45 = msg.ranges[66]
+		left_distance = msg.ranges[132]
 		self.get_logger().info(f'Forward distance: {forward_distance:.2f} meters')
 		self.get_logger().info(f'Right distance: {right_distance:.2f} meters')
 		self.get_logger().info(f'Right 45 distance: {right_45:.2f} meters')
@@ -40,16 +40,12 @@ class DriveLap(Node):
 		elif (forward_distance < 1.3 and right_distance < 1.5) or (self.cur_dir == "right" and left_distance > 2.0 and forward_distance < 1.0) or (right_45 < 0.8):
 			print("Left")
 			self.cur_dir = "left"
-			#for x in range(5):
-			self.go_left()
+			for x in range(10):
+				self.go_left()
 		elif (right_distance < 0.4 and forward_distance > 2.0):
 			print("Stabilize")
 			self.cur_dir = "stabilize"
-			self.stabilizeRight()
-		elif (right_distance > 0.4 and forward_distance > 2.0):
-			print("Stabilize")
-			self.cur_dir = "stabilize"
-			self.stabilizeLeft()
+			self.stabilize()
 		elif (right_distance > 0.35 and right_distance < 1.0):
 			print("Straight")
 			self.cur_dir = "straight"
@@ -57,8 +53,8 @@ class DriveLap(Node):
 		elif (right_distance > 1.5) or (left_45 < 0.8):
 			print("Right")
 			self.cur_dir = "right"
-			#for x in range(5):
-			self.go_right()
+			for x in range(10):
+				self.go_right()
 		else:
 			print("Default Straight")
 			self.cur_dir = "straight"
@@ -67,31 +63,25 @@ class DriveLap(Node):
 	def go_straight(self):
 		input = ServoCtrlMsg()
 		input.angle = 0.0
-		input.throttle = -0.6
+		input.throttle = 0.6
 		self.cmd_vel_publisher.publish(input)
 
 	def go_right(self):
 		input = ServoCtrlMsg()
-		input.angle = 1.0
-		input.throttle = -0.6
+		input.angle = -0.9
+		input.throttle = 0.6
 		self.cmd_vel_publisher.publish(input)
 	
 	def go_left(self):
 		input = ServoCtrlMsg()
-		input.angle = -1.0
-		input.throttle = -0.6
+		input.angle = 0.9
+		input.throttle = 0.6
 		self.cmd_vel_publisher.publish(input)
 
-	def stabilizeRight(self):
+	def stabilize(self):
 		input = ServoCtrlMsg()
-		input.angle = -0.1
-		input.throttle = -0.6
-		self.cmd_vel_publisher.publish(input)
-
-	def stabilizeLeft(self):
-		input = ServoCtrlMsg()
-		input.angle = 0.1
-		input.throttle = -0.6
+		input.angle = 0.2
+		input.throttle = 0.6
 		self.cmd_vel_publisher.publish(input)
 
 	def stop(self):
