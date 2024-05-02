c9ed9d89d92e6359e5aaac857da02bca2e60b911
mimu5583 <89423590+mimu5583@users.noreply.github.com>
2024-05-02 13:47:02 -0600

    Update driveLap.py

--- driveLap/driveLap/driveLap.py
+++ driveLap/driveLap/driveLap.py
@@ -15,48 +15,42 @@ class DriveLap(Node):
 			ServoCtrlMsg,
 			'/ctrl_pkg/servo_msg',
 			10)
-		self.forward_distance_ = 0.0
-		self.right_distance_ = 0.0
-		self.left_distance_ = 0.0
+		self.forward_distance = 0.0
+		self.right_distance = 0.0
+		self.left_distance = 0.0
 		self.cur_dir = "none"
 
 	def lidar_callback(self, msg):
-		#Print first 30 readings
-		#first_30 = msg.ranges[:30]
-		#self.get_logger().info(f'First 30 LiDAR readings: {first_30}')
-
-        	# Print the last 30 readings
-		last_30 = msg.ranges[-15:]
-		self.get_logger().info(f'Last 15 LiDAR readings: {last_30}')
-		
-		forward_distance_ = msg.ranges[0]
+		forward_distance = msg.ranges[0]
 		right_45 = msg.ranges[464]
-		right_distance_ = msg.ranges[398]
+		right_distance = msg.ranges[398]
 		left_45 = msg.ranges[66]
-		left_distance_ = msg.ranges[132]
-		#self.get_logger().info(f'Forward distance: {forward_distance_:.2f} meters')
-		#self.get_logger().info(f'Right distance: {right_distance_:.2f} meters')
-		#self.get_logger().info(f'Left distance: {left_distance_:.2f} meters')
+		left_distance = msg.ranges[132]
+		self.get_logger().info(f'Forward distance: {forward_distance:.2f} meters')
+		self.get_logger().info(f'Right distance: {right_distance:.2f} meters')
+		self.get_logger().info(f'Right 45 distance: {right_45:.2f} meters')
+		self.get_logger().info(f'Left distance: {left_distance:.2f} meters')
+		self.get_logger().info(f'Left 45 distance: {left_45:.2f} meters')
 
-		if forward_distance_ < 0.2:
-			#print("Stop")
+		if forward_distance < 0.2:
+			print("Stop")
 			self.cur_dir = "stop"
 			self.stop()
 		# or (self.cur_dir == "right" and left_distance_ > 2.0 and forward_distance_ < 2.0)
-		elif (forward_distance_ < 1.3 and right_distance_ < 1.5) or (self.cur_dir == "right" and left_distance_ > 2.0 and forward_distance_ < 1.0):
-			#print("Left")
+		elif (forward_distance < 1.3 and right_distance < 1.5) or (self.cur_dir == "right" and left_distance > 2.0 and forward_distance < 1.0) or (right_45 < 0.5):
+			print("Left")
 			self.cur_dir = "left"
 			self.go_left()
-		elif (right_distance_ < 0.4 and forward_distance_ > 2.0):
-			#print("Stabilize")
+		elif (right_distance < 0.4 and forward_distance > 2.0):
+			print("Stabilize")
 			self.cur_dir = "stabilize"
 			self.stabilize()
-		elif (right_distance_ > 0.35 and right_distance_ < 1.0):
-			#print("Straight")
+		elif (right_distance > 0.35 and right_distance < 1.0):
+			print("Straight")
 			self.cur_dir = "straight"
 			self.go_straight()
-		elif (right_distance_ > 1.5):
-			#print("Right")
+		elif (right_distance > 1.5) or (left_45 < 0.5):
+			print("Right")
 			self.cur_dir = "right"
 			self.go_right()
 
