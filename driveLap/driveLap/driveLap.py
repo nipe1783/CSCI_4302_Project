492491223f01d510186fbe2769955771b5402356
mimu5583 <89423590+mimu5583@users.noreply.github.com>
2024-05-03 14:36:02 -0600

    Update driveLap.py

--- driveLap/driveLap/driveLap.py
+++ driveLap/driveLap/driveLap.py
@@ -38,7 +38,7 @@ class DriveLap(Node):
 			self.cur_dir = "stop"
 			self.stop()
 		# or (self.cur_dir == "right" and left_distance_ > 2.0 and forward_distance_ < 2.0)
-		elif (forward_distance < 1.6 and right_distance < 1.5) or (self.cur_dir == "right" and left_distance > 2.0 and forward_distance < 1.0) or (right_45 < 1.0):
+		elif (forward_distance < 1.6 and right_distance < 1.5) or (self.cur_dir == "right" and left_distance > 2.0 and forward_distance < 1.0) or (right_45 < 0.5):
 			print("Left")
 			self.cur_dir = "left"
 			for x in range(10):
@@ -51,7 +51,7 @@ class DriveLap(Node):
 			print("Straight")
 			self.cur_dir = "straight"
 			self.go_straight()
-		elif (right_distance > 1.5) or (left_45 < 1.0):
+		elif (right_distance > 1.5) or (left_45 < 0.5):
 			print("Right")
 			self.cur_dir = "right"
 			for x in range(10):
