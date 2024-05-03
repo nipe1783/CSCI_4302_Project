2d4693888c200296e492a074a3e80bbfd87d8665
mimu5583 <89423590+mimu5583@users.noreply.github.com>
2024-05-03 16:11:34 -0600

    Update reverseDriving.py

--- reverseDriving/reverseDriving/reverseDriving.py
+++ reverseDriving/reverseDriving/reverseDriving.py
@@ -40,7 +40,7 @@ class DriveLap(Node):
 			self.cur_dir = "stop"
 			self.stop()
 		# or (self.cur_dir == "right" and left_distance_ > 2.0 and forward_distance_ < 2.0)
-		elif (forward_distance < 2.5 and right_distance < 2.5) or (self.cur_dir == "right" and left_distance > 2.0 and forward_distance < 1.0) or (right_45 < 1.5):
+		elif (forward_distance < 2.5 and right_distance < 2.0) or (self.cur_dir == "right" and left_distance > 2.0 and forward_distance < 1.0) or (right_45 < 1.5):
 			print("Left")
 			self.cur_dir = "left"
 			self.go_left()
