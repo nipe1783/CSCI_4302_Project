985b28c021f220d67b7b19b9465151d824b552ef
austinsmonell <97647088+austinsmonell@users.noreply.github.com>
2024-05-03 17:51:21 -0600

    Update avoidObstacle.py

--- avoidObstacle/avoidObstacle/avoidObstacle.py
+++ avoidObstacle/avoidObstacle/avoidObstacle.py
@@ -46,7 +46,7 @@ class DriveLap(Node):
 		#dl = msg.ranges[132]
 		
 		dl = float('inf')
-		for k  in range(132-90, 132+10, 10):
+		for k  in range(132-10, 132+90, 10):
 			dl_temp = 0.01
 			l = 0.000001
 			for i in range(k-5, k+5, 10):
