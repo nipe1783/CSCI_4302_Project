deea34bdedbb9b0908d18da2cbe5d92736a818b1
Ruan Abarbanel <13ruan13@gmail.com>
2024-05-03 19:17:19 -0600

    ytf

--- avoidObstacle/avoidObstacle/avoidObstacle.py
+++ avoidObstacle/avoidObstacle/avoidObstacle.py
@@ -46,7 +46,7 @@ class DriveLap(Node):
 		#dl = msg.ranges[132]
 		
 		dl = float('inf')
-		for k  in range(132-0, 132+50, 10):
+		for k  in range(132-0, 132+90, 10):
 			dl_temp = 0.01
 			l = 0.000001
 			for i in range(k-5, k+5, 10):
@@ -80,8 +80,8 @@ class DriveLap(Node):
 		k_th_d = 0.0
 		k_th_i = 0.0
 
-		k_psi_p = -0.6
-		k_psi_d = 0.1
+		k_psi_p = -0.7
+		k_psi_d = 0.02
 		k_psi_i = 0.0
 
 		#P
