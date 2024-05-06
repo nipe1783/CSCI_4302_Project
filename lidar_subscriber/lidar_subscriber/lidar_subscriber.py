1f5f4707465a20dbd61ffc9f9b5fd72ff602e9f3
mimu5583 <89423590+mimu5583@users.noreply.github.com>
2024-05-06 17:49:12 -0600

    Update lidar_subscriber.py

--- lidar_subscriber/lidar_subscriber/lidar_subscriber.py
+++ lidar_subscriber/lidar_subscriber/lidar_subscriber.py
@@ -2,10 +2,11 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from deepracer_interfaces_pkg.msg import ServoCtrlMsg
+import time
 
-class LidarSubscriber(Node):
+class DriveLap(Node):
 	def __init__(self):
-		super().__init__('lidar_subscriber')
+		super().__init__('driveLap')
 		self.subscription = self.create_subscription(
 			LaserScan,
 			'/rplidar_ros/scan',
@@ -13,49 +14,106 @@ class LidarSubscriber(Node):
 			10)
 		self.cmd_vel_publisher = self.create_publisher(
 			ServoCtrlMsg,
-			'/webserver_pkg/manual_drive',
+			'/ctrl_pkg/servo_msg',
 			10)
+		self.forward_distance = 0.0
+		self.right_distance = 0.0
+		self.left_distance = 0.0
+		self.max_throttle = 0.6
+		self.cur_dir = "none"
 
-	#This is where we should process the Lidar readings
 	def lidar_callback(self, msg):
-		print(len(msg.ranges))
-		forward_distance = self.get_forward_distance(msg)
+		forward_distance = msg.ranges[0]
+		right_45 = msg.ranges[464]
+		right_distance = msg.ranges[398]
+		left_45 = msg.ranges[66]
+		left_distance = msg.ranges[132]
 		self.get_logger().info(f'Forward distance: {forward_distance:.2f} meters')
-		if forward_distance > 1.0:
-			self.drive_forward()
-		else:
+		self.get_logger().info(f'Right distance: {right_distance:.2f} meters')
+		self.get_logger().info(f'Right 45 distance: {right_45:.2f} meters')
+		self.get_logger().info(f'Left distance: {left_distance:.2f} meters')
+		self.get_logger().info(f'Left 45 distance: {left_45:.2f} meters')
+
+		if forward_distance < 0.2:
+			print("Stop")
+			self.cur_dir = "stop"
 			self.stop()
+		# or (self.cur_dir == "right" and left_distance_ > 2.0 and forward_distance_ < 2.0)
+		elif (forward_distance < 2.0 and right_distance < 2.0) or (self.cur_dir == "right" and left_distance > 2.0 and forward_distance < 1.0) or (right_45 < 1.2):
+			print("Left")
+			self.cur_dir = "left"
+			self.go_left()
+			print("sleeping")
+			time.sleep(0.1)
+		elif (right_distance < 0.4 and forward_distance > 2.0):
+			print("Stabilize")
+			self.cur_dir = "stabilize"
+			self.stabilize()
+		elif (right_distance > 0.35 and right_distance < 1.0):
+			print("Straight")
+			self.cur_dir = "straight"
+			self.go_straight()
+		elif (right_distance > 1.5) or (left_45 < 1.2):
+			print("Right")
+			self.cur_dir = "right"
+			self.go_right()
+			print("sleeping")
+			time.sleep(0.1)
+		else:
+			print("Default Straight (turn slightly right)")
+			self.cur_dir = "stabilize"
+			self.stabilizeRight()
 
-	def get_forward_distance(self, scan: LaserScan) -> float:
-		forward_distance = scan.ranges[0]
-		# Filtering out invalid measurements
-		if forward_distance == float('inf') or forward_distance == 0.0:
-			return -1.0
-		return forward_distance
+	def go_straight(self):
+		input = ServoCtrlMsg()
+		input.angle = 0.0
+		input.throttle = self.max_throttle
+		self.cmd_vel_publisher.publish(input)
 
-	def drive_forward(self) -> float:
-		msg = ServoCtrlMsg()
-		msg.angle = 0.0 #no steer
-		msg.throttle = 0.6
-		self.cmd_vel_publisher.publish(msg)
+	def stabilizeRight(self):
+		input = ServoCtrlMsg()
+		input.angle = -0.05
+		input.throttle = self.max_throttle
+		self.cmd_vel_publisher.publish(input)
+
+	def go_right(self):
+		input = ServoCtrlMsg()
+		input.angle = -0.6
+		input.throttle = self.max_throttle
+		self.cmd_vel_publisher.publish(input)
+	
+	def go_left(self):
+		input = ServoCtrlMsg()
+		input.angle = 0.6
+		input.throttle = self.max_throttle
+		self.cmd_vel_publisher.publish(input)
+
+	def stabilize(self):
+		input = ServoCtrlMsg()
+		input.angle = 0.05
+		input.throttle = self.max_throttle
+		self.cmd_vel_publisher.publish(input)
 
 	def stop(self):
-		msg = ServoCtrlMsg()
-		msg.angle = 0.0
-		msg.throttle = 0.0
-		self.cmd_vel_publisher.publish(msg)
+		input = ServoCtrlMsg()
+		input.angle = 0.0
+		input.throttle = 0.0
+		self.cmd_vel_publisher.publish(input)
 
 def main(args = None):
     rclpy.init(args=args)
-    node = LidarSubscriber()
+    node = DriveLap()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
-        pass
+        node.stop()
     finally:
         # Destroy the node explicitly
         node.destroy_node()
         rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
 
 if __name__ == '__main__':
     main()
