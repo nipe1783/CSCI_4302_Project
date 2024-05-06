7fda2f93c78acac91c0522c707c81113ff10efb5
mimu5583 <89423590+mimu5583@users.noreply.github.com>
2024-05-06 15:23:19 -0600

    Update driveLap.py

--- driveLap/driveLap/driveLap.py
+++ driveLap/driveLap/driveLap.py
@@ -55,9 +55,12 @@ class DriveLap(Node):
 			print("Stop")
 			self.stop()
 		# or (self.cur_dir == "right" and left_distance_ > 2.0 and forward_distance_ < 2.0)
-		elif forward_distance < 1.8 and right_distance < 0.5:
-			print("Left")
-			self.go_left()
+		elif forward_distance < 1.8:# and right_distance < 0.5:
+		right_distance = msg.ranges[398]
+			while right_distance < 0.5:
+				right_distance = msg.ranges[398]
+				print("Left")
+				self.go_left()
 			#print("sleeping")
 			#time.sleep(0.1)
 		else:
