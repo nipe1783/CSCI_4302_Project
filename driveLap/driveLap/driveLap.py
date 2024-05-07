f767797bc08cbfd86c91c0a86534478d57d93186
austinsmonell <97647088+austinsmonell@users.noreply.github.com>
2024-05-06 18:45:39 -0600

    Update driveLap.py

--- driveLap/driveLap/driveLap.py
+++ driveLap/driveLap/driveLap.py
@@ -62,7 +62,7 @@ class DriveLap(Node):
 			print("Stop")
 			self.stop()
 		# or (self.cur_dir == "right" and left_distance_ > 2.0 and forward_distance_ < 2.0)
-		elif forward_distance < 1.7 and right_15 < 1.8:# and right_distance < 0.5:
+		elif forward_distance < 1.8 and right_15 < 1.8:# and right_distance < 0.5:
 			#self.avoid_toggle = 1
 			#right_distance = msg.ranges[398]
 			print("Left")
